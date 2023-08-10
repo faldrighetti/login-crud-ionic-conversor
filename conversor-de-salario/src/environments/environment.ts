ac4f4dd4fede9cea4cac54a34235bbdd27bc757c
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-08-10 12:45:32 -0300

    Instala firebase para la database
    Agrega firebase al environment para empezar a loguear al usuario en la base de datos de la app con email.

--- conversor-de-salario/src/environments/environment.ts
+++ conversor-de-salario/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCICxZWq6kWFe2UayDpm9AJCRBmESNOq_g",
+    authDomain: "ionic-conversor-salario.firebaseapp.com",
+    projectId: "ionic-conversor-salario",
+    storageBucket: "ionic-conversor-salario.appspot.com",
+    messagingSenderId: "575239332853",
+    appId: "1:575239332853:web:1afa9966b78546ef6a76bc"
+  }
 };
 
 /*
